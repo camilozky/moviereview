e8b2453364ac85aeda5ed68a12c3960afcded96f
Milo <juan.rendon@globant.com>
2019-10-04 09:31:40 -0500

    var BASEURL and APIKEY Added into open class Constants

--- app/src/main/java/com/globant/moviereview/utils/Constants.kt
+++ app/src/main/java/com/globant/moviereview/utils/Constants.kt
@@ -14,6 +14,8 @@ open class Constants {
         const val GRILL_LAYOUT: Int = 2
         const val STAGGERED_LAYOUT: Int = 3
         const val ID_MOVIE: String = "idMovie"
+        var BASEURL = "https://api.themoviedb.org/3/"
+        var APIKEY = "99ac1d44af506e889c0cb61a2ef3fa22"
         const val VOTE_MAX = 10
         const val RATING_MAX = 5
     }
