15ed9eaa04743b46c59b352da0d84826258b6ef7
Milo <juan.rendon@globant.com>
2019-10-08 09:18:49 -0500

    comments to respective functions within interface MovieDao added

--- app/src/main/java/com/globant/moviereview/model/MovieDao.kt
+++ app/src/main/java/com/globant/moviereview/model/MovieDao.kt
@@ -7,26 +7,36 @@ import androidx.room.Query
 
 /**
  * MovieDao
+ *
+ * to Access Database @Entity(tableName = "movies") data class MovieReview
  * facilitates access to stored data with the following methods
- * insertMovies -> inserts a list of all movies
- * insertMovie -> insert a movie
- * getMovies -> consult a list of all the movies
- * getMovieDetail -> consult a movie
  *
  * @author david.mazo
  */
 
 @Dao
 interface MovieDao {
+    /**
+     * insertMovie -> insert a movie into Database movies
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertMovie(movie: MovieReview)
 
+    /**
+     * getMovies -> consult a list of all the movies
+     */
     @Query("SELECT * from movies")
     fun getMovies(): List<MovieReview>
 
+    /**
+     * getMovieDetail -> consult a movie
+     */
     @Query("SELECT * FROM movies WHERE movies.id=:id")
     fun getMovieDetail(id: Int): MovieReview
 
+    /**
+     * deleteMovies-> delete all rows
+     */
     @Query("DELETE from movies")
     fun deleteMovies()
 }
