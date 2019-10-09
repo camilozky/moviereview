fff5199411fb3560570df3e5cc2760b537ddd15a
Milo <juan.rendon@globant.com>
2019-10-09 15:41:29 -0500

    movie_border.xml created detail_item.xml rearranged

--- app/src/main/java/com/globant/moviereview/ui/DetailMovieActivity.kt
+++ app/src/main/java/com/globant/moviereview/ui/DetailMovieActivity.kt
@@ -10,14 +10,7 @@ import com.globant.moviereview.R
 import com.globant.moviereview.model.MovieDatabase.Companion.getMovieDatabase
 import com.globant.moviereview.model.MovieReview
 import com.globant.moviereview.utils.Constants.Companion.ID_MOVIE
-import kotlinx.android.synthetic.main.detail_item.image_view_ic_star
-import kotlinx.android.synthetic.main.detail_item.image_view_movie_picture
-import kotlinx.android.synthetic.main.detail_item.rating_bar_average
-import kotlinx.android.synthetic.main.detail_item.text_view_average
-import kotlinx.android.synthetic.main.detail_item.text_view_movie_title
-import kotlinx.android.synthetic.main.detail_item.text_view_summary
-import kotlinx.android.synthetic.main.detail_item.text_view_title_original
-import kotlinx.android.synthetic.main.detail_item.text_view_title_release_date
+import kotlinx.android.synthetic.main.detail_item.*
 
 
 /**
@@ -42,7 +35,6 @@ class DetailMovieActivity : AppCompatActivity() {
 
     private fun loadMovieReview(movieReview: MovieReview) {
         text_view_movie_title.text = movieReview.title
-        text_view_title_original.text = movieReview.originalTitle
         text_view_title_release_date.text = movieReview.releaseDate
         text_view_summary.text = movieReview.summary
         Glide.with(image_view_movie_picture)
